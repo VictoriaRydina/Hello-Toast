abb2da3cd718893358f73fc8ac4b17d34d69dfb6
VictoriaRyd <victoriarydina@gmail.com>
2022-04-04 11:36:02 +0300

    Added onClick handlers for buttons

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.hellotoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCount: Int = 0
+    private lateinit var showCount : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showCount = findViewById(R.id.show_count)
+    }
+
+    fun showToast(view: View) {
+        val toast = Toast.makeText(this, R.string.toast_message,
+            Toast.LENGTH_SHORT)
+        toast.show()
+    }
+    fun countUp(view: View) {
+        mCount++
+        if (showCount != null)
+            showCount.setText(Integer.toString(mCount))
     }
 }
